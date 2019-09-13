63a3d7604790a453800b8540c50f6e321987eb2b
Kevin <hyoungkyeu@gmail.com>
2019-09-13 00:19:19 -0500

    Fixed the issue where isStartPosition and isEndPosition functions did not work

--- js/generateGrid.js
+++ js/generateGrid.js
@@ -1,9 +1,19 @@
-//Use backticks (`) for template literals!
-width = window.innerWidth;
-height = window.innerHeight;
-side = 30;
+$(document).ready(function () {
+    //Use backticks (`) for template literals!
+    width = window.innerWidth;
+    height = window.innerHeight;
+    side = 30;
 
-console.log(width, height);
+    console.log(width, height);
+    console.log("welcome!");
+    //generates grid
+    generateGrid();
+    startPosition(5, 10);
+    endPosition(5, 35);
+
+    console.log(isStartPosition(5, 10));
+    console.log(isEndPosition(5, 35));
+});
 
 function generateGrid() {
     /*
@@ -21,7 +31,7 @@ function generateGrid() {
 
     for (let r = 0; r < height; r += side) {
         for (let c = 0; c < width - 2 * side; c += side) {
-            tableHTML += `<rect id="${r/side}-${c/side}" x="${c}" y="${r}" width="${side}" height="${side}" r="0" rx="0" ry="0" fill="#ffffff" stroke="#000" stroke-opcaticy="0.2" style="-webkit-tap-highlight-color:rgba(0, 0, 0, 0)"></rect>`;
+            tableHTML += `<rect id="${r / side}-${c / side}" x="${c}" y="${r}" width="${side}" height="${side}" r="0" rx="0" ry="0" fill="#ffffff" stroke="#000" stroke-opcaticy="0.2" style="-webkit-tap-highlight-color:rgba(0, 0, 0, 0)"></rect>`;
             // <rect x="0" y="0" width="30" height="30" r="0" rx="0" ry="0" fill="#ffffff" stroke="#000" stroke-opcaticy="0.2" style="-webkit-tap-highlight-color:rgba(0, 0, 0, 0)"></rect>
         }
     }
@@ -38,22 +48,18 @@ function getheight() {
     return height;
 }
 
-function startPosition(row, col){
+function startPosition(row, col) {
     document.getElementById(`${row}-${col}`).setAttribute("fill", "green");
 }
 
-function endPosition(row, col){
+function endPosition(row, col) {
     document.getElementById(`${row}-${col}`).setAttribute("fill", "red");
 }
 
-function isStartPosition(row, col){
-    return $(`#${row}-${col}`).filter(function(){
-        $(this).attr("fill") === ("green");
-    });
+function isStartPosition(row, col) {
+    return document.getElementById(`${row}-${col}`).getAttribute("fill") == "green";
 }
 
-function isEndPosition(row, col){
-    return $(`#${row}-${col}`).filter(function(){
-        $(this).attr("fill") === ("red");
-    });
+function isEndPosition(row, col) {
+    return document.getElementById(`${row}-${col}`).getAttribute("fill") == "red";
 }
--- js/mouseEvents.js
+++ js/mouseEvents.js
@@ -1,13 +1,4 @@
 $(document).ready(function () {
-    console.log("welcome!");
-    //generates grid
-    generateGrid();
-    startPosition(5,10);
-    endPosition(5,35);
-
-    console.log(isStartPosition(5,10));
-
-
     //When mouse is down
     $(document.body).on("mousedown", function () {
         console.log("mouse is down!");
@@ -33,14 +24,10 @@ function enableBlocks() {
     let col = Math.floor((event.clientX - 8) / side);
     let row = Math.floor((event.clientY - 8) / side);
 
-    if(document.getElementById(`${row}-${col}`).style != "red"){
-        document.getElementById(`${row}-${col}`).setAttribute("fill", "#333");
-    }
-    // console.log(`Row: ${row} \n Col: ${col}`);
-    // console.log("Hover on a rect!");
+    document.getElementById(`${row}-${col}`).setAttribute("fill", "#333");
 }
 
-function disableBlocks(){
+function disableBlocks() {
     console.log("mouseover is disabled!");
     $("svg").off("mouseover");
 }
