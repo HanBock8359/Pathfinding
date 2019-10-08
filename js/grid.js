5bb277bc5b9fe748e601d359b47185914f60145c
Kevin <hyoungkyeu@gmail.com>
2019-10-07 21:28:07 -0500

    Minor change

--- js/grid.js
+++ js/grid.js
@@ -6,6 +6,7 @@ var grid = {
     side: 30,
     delay: 50,
     isDfsDone: false,
+    offset: 8 * 4,
 
     generateGrid: function () {
         /*
@@ -84,8 +85,8 @@ var grid = {
     },
 
     enableBlocks: function (event) {
-        let col = Math.floor((event.pageX - 8) / this.side);
-        let row = Math.floor((event.pageY - 8) / this.side);
+        let col = Math.floor((event.pageX - this.offset) / this.side);
+        let row = Math.floor((event.pageY - this.offset) / this.side);
         let ele = document.getElementById(`${row}-${col}`);
 
         //Checks if the user is clicking on the start or end position
@@ -99,8 +100,8 @@ var grid = {
     },
 
     disableBlocks: function (event) {
-        let col = Math.floor((event.pageX - 8) / this.side);
-        let row = Math.floor((event.pageY - 8) / this.side);
+        let col = Math.floor((event.pageX - this.offset) / this.side);
+        let row = Math.floor((event.pageY - this.offset) / this.side);
         let ele = document.getElementById(`${row}-${col}`);
 
         //Checks if the user is clicking on the start or end position
